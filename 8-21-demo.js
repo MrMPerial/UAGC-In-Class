e2ef72b01b960e831476ae065852411b83436592
MrMPerial <christopher.mitchell26@gmail.com>
2017-08-21 20:17:32 -0500

    Stage two

--- 8-21-demo.js
+++ 8-21-demo.js
@@ -1,22 +1,26 @@
 let http = require('http');
 let fs = require('fs');
 
+let mod = require('./module');
+
 let server = http.createServer((req, res) => {
-  fs.writeFile('newTextFile.txt', '', (err) => {
-    if(err) throw err;
+  mod.createFile('newTextFile.txt')
+  .then(() => {
     console.log('File has successfully been created.');
-
-    fs.appendFile('newTextFile.txt', 'New Text', (err) => {
-      if(err) throw err;
-      console.log('Data was successfully appended to the file.');
-
-      fs.readFile('newTextFile.txt', 'utf-8', (err, data) => {
-        if(err) throw err;
-        res.write(data);
-        res.end();
-      });
-    });
-  });
+    return mod.appendFile('newTextFile.txt', 'More Text')
+  })
+  .then(() => {
+    console.log('Data was successfully appended to the file.');
+    return mod.readFile('newTextFile.txt', 'utf-8')
+  })
+  .then((data) => {
+    console.log('Here\'s Johnny');
+    res.write(data);
+    res.end();
+  })
+  .catch((err) => {
+    return err;
+  })
 });
 
 let port = fs.readFileSync('./port.txt', 'utf-8');
