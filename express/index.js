14c85100dcfe5ced4f09a2b05a0badadac42bed0
MrMPerial <christopher.mitchell26@gmail.com>
2017-09-02 14:03:22 -0500

    Created middleware

--- express/index.js
+++ express/index.js
@@ -4,9 +4,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const myMiddleware = (req, res, next) => {
+  console.log('Hello MW');
+  next();
+};
 
-app.use(bodyParser.json());
-app.use('/', routes);
+app.get('/', (req, res) => {
+  res.status(200).send('My root');
+});
+
+//app.use(bodyParser.json());
+//app.use('/', routes);
 
 app.listen(3000, () => {
   console.log('Puppies and Kitties Application listening on port 3000');
