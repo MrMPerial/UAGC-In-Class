d00998f112d31e1ff14325906d9cc3ee8a2dc56e
MrMPerial <christopher.mitchell26@gmail.com>
2017-09-06 20:40:25 -0500

    9-6

--- express/index.js
+++ express/index.js
@@ -6,7 +6,12 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use('/', routes);
+app.get('/tacos', (req, res) => {
+  res.send('It worked!');
+});
 
 app.listen(3000, () => {
   console.log('Puppies and Kitties Application listening on port 3000');
 });
+
+module.exports = app;
