3d24e47eb6b0b5895f7c1859a3b3c5fe26fa00cf
MrMPerial <christopher.mitchell26@gmail.com>
2017-09-25 18:27:59 -0500

    Added middleware

--- users/index.js
+++ users/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const users = require('./users');
 const helpers = require('./helpers');
+const errMiddleware = require('./err-middleware');
 
 const app = express();
 
@@ -14,17 +15,23 @@ app.get('/', (req, res) => {
   res.status(200).json(users);
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', (req, res, next) => {
   let user;
   for (let i = 0; i < users.length; i++) {
     if ( req.params.id.toString() === users[i].id.toString() ) {
       user = users[i];
     }
   }
-  res.status(200).json(user);
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    next('this user does not exist');
+  }
+  
 });
 
-app.post('/', (req, res) => {
+app.post('/', (req, res, next) => {
   let userCopy = Object.assign({}, req.body);
   let errorMessage = "You screwed up";
 
@@ -34,11 +41,12 @@ app.post('/', (req, res) => {
     users.push(userCopy);
     res.status(200).json(userCopy);
   } else {
-    res.status(401).send(errorMessage);
+    next('invalid user data supplied');
   }
 
 });
 
+app.use(errMiddleware);
 app.listen(3001, () => {
   console.log("Listening on 3001");
 });
