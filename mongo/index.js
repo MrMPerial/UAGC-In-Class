877ea8be14ce57354bfab8ad5c246e7f63d19ffe
MrMPerial <christopher.mitchell26@gmail.com>
2017-10-14 14:51:22 -0500

    Express to Mongo

--- mongo/index.js
+++ mongo/index.js
@@ -1,62 +1,43 @@
-const MongoClient = require('mongodb').MongoClient;
+const express = require('express');
+const mongodb = require('./mongodb.utils');
+const Puppy = require('./puppy.model');
+const bodyParser = require('body-parser');
 
-const uri = 'mongodb://localhost:27017/dbName';
+const app = express();
 
-MongoClient.connect(uri, (err, db) => {
-  if (err) {
-    console.log(`Error connecting to database: ${err}`);
-  }
+app.use(bodyParser.json());
 
-  console.log('Connected successfully to database');
+mongodb.createEventListeners();
+mongodb.connect();
 
-  insertDocuments(db, () => {
-    updateDocuments(db, () => {
-      removeDocuments(db, () => {
-        findAllDocuments(db, () => {
-          db.close();
-        });
-      });      
-    });
+app.get('/', (req, res) => {
+  Puppy.find({}).exec()
+  .then((results) => {
+    console.log(results);
+    res.status(200).send(results);
+  }).catch((err) => {
+    console.log(err);
+    res.status(404);
   });
 });
 
-function insertDocuments(db, callback) {
-  const collection = db.collection('node-test');
-  const data = [{a: 1},{a: 2},{a: 3}];
-
-  collection.insertMany(data, (err, results) => {
-    if (err) {
-      console.log(`Unable to insert documents: ${data}`);
-    }
-    console.log('Inserted documents into collection');
-    callback(results);
+app.post('/puppies', (req, res) => {
+  const puppy = new Puppy({
+    name: req.body.name,
+    breed: req.body.breed,
+    age: req.body.age,
+    likes: req.body.likes,
+    dislikes: req.body.dislikes
   });
-}
 
-function findAllDocuments(db, callback) {
-  const collection = db.collection('node-test');
-
-  collection.find({}).toArray((err, docs) => {
-    console.log('Found the following records:');
-    console.log(docs);
-    callback(docs);
+  puppy.save()
+  .then((results) => {
+    console.log(results);
+  }).catch((err) => {
+    throw(err);
   });
-}
+});
 
-function updateDocuments(db, callback) {
-  const collection = db.collection('node-test');
-
-  collection.updateOne({ a : 2}, { $set: { b : 1 } }, (err, results) => {
-    console.log('Updated the document with the field a equal to 2');
-    callback(results);
-  });
-}
-
-function removeDocuments(db, callback) {
-  const collection = db.collection('node-test');
-
-  collection.deleteOne({ a : 3 }, (err, results) => {
-    console.log('Removed document where a equal to 3');
-    callback(results);
-  });
-}
+app.listen(3000, () => {
+  console.log('Listening on 3000');
+});
